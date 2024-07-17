a08789c2c4818edbca430c9089eea4feeb0efcfa
[mahen290] <[mahendraxx@gmail.com]>
2024-07-17 18:31:00 +0530

    Major Changes useState Hook Introduced In This Project

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import './SearchBox.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function SearchBox() {
+
+  const [ city, setCity ] = useState(" ");
+  const CityChanger = (event) => {
+    console.log(event);
+    setCity(event.target.value);
+  };
+
+  const HandleSubmit = (event) => {
+    event.preventDefault();
+    console.log(city);
+    setCity(" ");
+  };
+
   return (
     <div>
-      <h2> Search For Current Status Of WEATHER </h2>
+      <h2> Search Current Status Of WEATHER </h2>
       <div className = "SearchForm">
-      <form >
-        <TextField id = "city" label = "city name" variant = "outlined" required />
+      <form onSubmit = { HandleSubmit }>
+        <TextField id = "city" label = "city name" variant = "outlined" value = { city } onChange = { CityChanger } required />
         <br /> <br />
         <Button variant = "contained" type = "submit"> SUBMIT </Button>
       </form>
       </div> 
     </div>
-  )
+  );
 }
 
 export default SearchBox;
